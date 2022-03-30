3c9e0b985c458dc49a14df8c334718b00f8a2322
ptnguyen617 <ptnguyen617@yahoo.com>
2022-03-30 19:46:48 -0400

    Commit Exercise 4 Result

--- app.js
+++ app.js
@@ -12,3 +12,17 @@ Exercise 2:
 */
 foods.push("pizza", "cheeseburger");
 console.log("Exercise 2 Result: ", foods);
+/*
+Exercise 3:
+  - Add the string 'taco' to the foods array so that 'taco' is the first food in the array.
+	- How do you add things to the front of an array, push wont work so what do you do?
+*/
+foods.unshift("taco");
+console.log("Exercise 3 Result: ", foods);
+/*
+Exercise 4:
+  - Access the string 'pizza' with bracket notation (based upon its known position) in the foods array and assign to a variable named favFood.
+	- You should end up with a variable called favorite food that is equal to 'pizza'
+*/
+let favFood = foods [1];
+console.log("Exercise 4 Result: ", foods);
